3dd79d7abb094144e38954daf5072058b5b12624
Vishwa Sheth <vishwasheth03@yahoo.com>
2017-12-27 12:26:17 -0500

    Add files via upload

---
+++ Spring Launcher Formula Calculator/Spring Launcher Formula Calculator/Form1.Designer.vb
@@ -0,0 +1,421 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ProjectileHeightTextBox = New System.Windows.Forms.TextBox()
+        Me.SpringMassTextBox = New System.Windows.Forms.TextBox()
+        Me.EquilibriumLengthTextBox = New System.Windows.Forms.TextBox()
+        Me.AngleTextBox = New System.Windows.Forms.TextBox()
+        Me.HorizontalDistanceTextBox = New System.Windows.Forms.TextBox()
+        Me.TargetHeightTextBox = New System.Windows.Forms.TextBox()
+        Me.SpringConstantTextBox = New System.Windows.Forms.TextBox()
+        Me.ProjectileHeightButton = New System.Windows.Forms.Button()
+        Me.TargetHeightButton = New System.Windows.Forms.Button()
+        Me.HorizontalDistanceButton = New System.Windows.Forms.Button()
+        Me.AngleButton = New System.Windows.Forms.Button()
+        Me.EquilibriumLenghtButton = New System.Windows.Forms.Button()
+        Me.SpringMassButton = New System.Windows.Forms.Button()
+        Me.SpringConstantButton = New System.Windows.Forms.Button()
+        Me.ProjectileHeightLabel = New System.Windows.Forms.Label()
+        Me.HorizontalDistanceLabel = New System.Windows.Forms.Label()
+        Me.TargetHeightLabel = New System.Windows.Forms.Label()
+        Me.SpringConstantLabel = New System.Windows.Forms.Label()
+        Me.SpringMassLabel = New System.Windows.Forms.Label()
+        Me.EquilibriumLengthLabel = New System.Windows.Forms.Label()
+        Me.AngleLabel = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.xPullDistanceLabel = New System.Windows.Forms.Label()
+        Me.LaunchSpeedLabel = New System.Windows.Forms.Label()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(125, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(232, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Initial height of spring/projectile (m):"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(224, 231)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(133, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Mass of spring (kg):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(149, 190)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(208, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Equilibrium length of spring (m):"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(191, 148)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(166, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Angle of ramp (degrees):"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 106)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(345, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Horizontal distance between projectile and target (m):"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(222, 64)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(135, 17)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Height of target (m):"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(207, 274)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(150, 17)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Spring constant (N/m):"
+        '
+        'ProjectileHeightTextBox
+        '
+        Me.ProjectileHeightTextBox.Location = New System.Drawing.Point(363, 22)
+        Me.ProjectileHeightTextBox.Name = "ProjectileHeightTextBox"
+        Me.ProjectileHeightTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.ProjectileHeightTextBox.TabIndex = 8
+        '
+        'SpringMassTextBox
+        '
+        Me.SpringMassTextBox.Location = New System.Drawing.Point(363, 231)
+        Me.SpringMassTextBox.Name = "SpringMassTextBox"
+        Me.SpringMassTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.SpringMassTextBox.TabIndex = 9
+        '
+        'EquilibriumLengthTextBox
+        '
+        Me.EquilibriumLengthTextBox.Location = New System.Drawing.Point(363, 190)
+        Me.EquilibriumLengthTextBox.Name = "EquilibriumLengthTextBox"
+        Me.EquilibriumLengthTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.EquilibriumLengthTextBox.TabIndex = 10
+        '
+        'AngleTextBox
+        '
+        Me.AngleTextBox.Location = New System.Drawing.Point(363, 148)
+        Me.AngleTextBox.Name = "AngleTextBox"
+        Me.AngleTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.AngleTextBox.TabIndex = 11
+        '
+        'HorizontalDistanceTextBox
+        '
+        Me.HorizontalDistanceTextBox.Location = New System.Drawing.Point(363, 106)
+        Me.HorizontalDistanceTextBox.Name = "HorizontalDistanceTextBox"
+        Me.HorizontalDistanceTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.HorizontalDistanceTextBox.TabIndex = 12
+        '
+        'TargetHeightTextBox
+        '
+        Me.TargetHeightTextBox.Location = New System.Drawing.Point(363, 64)
+        Me.TargetHeightTextBox.Name = "TargetHeightTextBox"
+        Me.TargetHeightTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.TargetHeightTextBox.TabIndex = 13
+        '
+        'SpringConstantTextBox
+        '
+        Me.SpringConstantTextBox.Location = New System.Drawing.Point(363, 274)
+        Me.SpringConstantTextBox.Name = "SpringConstantTextBox"
+        Me.SpringConstantTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.SpringConstantTextBox.TabIndex = 14
+        '
+        'ProjectileHeightButton
+        '
+        Me.ProjectileHeightButton.Location = New System.Drawing.Point(470, 21)
+        Me.ProjectileHeightButton.Name = "ProjectileHeightButton"
+        Me.ProjectileHeightButton.Size = New System.Drawing.Size(75, 23)
+        Me.ProjectileHeightButton.TabIndex = 15
+        Me.ProjectileHeightButton.Text = "Enter"
+        Me.ProjectileHeightButton.UseVisualStyleBackColor = True
+        '
+        'TargetHeightButton
+        '
+        Me.TargetHeightButton.Location = New System.Drawing.Point(470, 63)
+        Me.TargetHeightButton.Name = "TargetHeightButton"
+        Me.TargetHeightButton.Size = New System.Drawing.Size(75, 23)
+        Me.TargetHeightButton.TabIndex = 16
+        Me.TargetHeightButton.Text = "Enter"
+        Me.TargetHeightButton.UseVisualStyleBackColor = True
+        '
+        'HorizontalDistanceButton
+        '
+        Me.HorizontalDistanceButton.Location = New System.Drawing.Point(469, 105)
+        Me.HorizontalDistanceButton.Name = "HorizontalDistanceButton"
+        Me.HorizontalDistanceButton.Size = New System.Drawing.Size(75, 23)
+        Me.HorizontalDistanceButton.TabIndex = 17
+        Me.HorizontalDistanceButton.Text = "Enter"
+        Me.HorizontalDistanceButton.UseVisualStyleBackColor = True
+        '
+        'AngleButton
+        '
+        Me.AngleButton.Location = New System.Drawing.Point(469, 147)
+        Me.AngleButton.Name = "AngleButton"
+        Me.AngleButton.Size = New System.Drawing.Size(75, 23)
+        Me.AngleButton.TabIndex = 18
+        Me.AngleButton.Text = "Enter"
+        Me.AngleButton.UseVisualStyleBackColor = True
+        '
+        'EquilibriumLenghtButton
+        '
+        Me.EquilibriumLenghtButton.Location = New System.Drawing.Point(469, 189)
+        Me.EquilibriumLenghtButton.Name = "EquilibriumLenghtButton"
+        Me.EquilibriumLenghtButton.Size = New System.Drawing.Size(75, 23)
+        Me.EquilibriumLenghtButton.TabIndex = 19
+        Me.EquilibriumLenghtButton.Text = "Enter"
+        Me.EquilibriumLenghtButton.UseVisualStyleBackColor = True
+        '
+        'SpringMassButton
+        '
+        Me.SpringMassButton.Location = New System.Drawing.Point(470, 230)
+        Me.SpringMassButton.Name = "SpringMassButton"
+        Me.SpringMassButton.Size = New System.Drawing.Size(75, 23)
+        Me.SpringMassButton.TabIndex = 20
+        Me.SpringMassButton.Text = "Enter"
+        Me.SpringMassButton.UseVisualStyleBackColor = True
+        '
+        'SpringConstantButton
+        '
+        Me.SpringConstantButton.Location = New System.Drawing.Point(469, 273)
+        Me.SpringConstantButton.Name = "SpringConstantButton"
+        Me.SpringConstantButton.Size = New System.Drawing.Size(75, 23)
+        Me.SpringConstantButton.TabIndex = 21
+        Me.SpringConstantButton.Text = "Enter"
+        Me.SpringConstantButton.UseVisualStyleBackColor = True
+        '
+        'ProjectileHeightLabel
+        '
+        Me.ProjectileHeightLabel.AutoSize = True
+        Me.ProjectileHeightLabel.Location = New System.Drawing.Point(552, 27)
+        Me.ProjectileHeightLabel.Name = "ProjectileHeightLabel"
+        Me.ProjectileHeightLabel.Size = New System.Drawing.Size(16, 17)
+        Me.ProjectileHeightLabel.TabIndex = 22
+        Me.ProjectileHeightLabel.Text = "0"
+        '
+        'HorizontalDistanceLabel
+        '
+        Me.HorizontalDistanceLabel.AutoSize = True
+        Me.HorizontalDistanceLabel.Location = New System.Drawing.Point(552, 111)
+        Me.HorizontalDistanceLabel.Name = "HorizontalDistanceLabel"
+        Me.HorizontalDistanceLabel.Size = New System.Drawing.Size(16, 17)
+        Me.HorizontalDistanceLabel.TabIndex = 24
+        Me.HorizontalDistanceLabel.Text = "0"
+        '
+        'TargetHeightLabel
+        '
+        Me.TargetHeightLabel.AutoSize = True
+        Me.TargetHeightLabel.Location = New System.Drawing.Point(552, 69)
+        Me.TargetHeightLabel.Name = "TargetHeightLabel"
+        Me.TargetHeightLabel.Size = New System.Drawing.Size(16, 17)
+        Me.TargetHeightLabel.TabIndex = 25
+        Me.TargetHeightLabel.Text = "0"
+        '
+        'SpringConstantLabel
+        '
+        Me.SpringConstantLabel.AutoSize = True
+        Me.SpringConstantLabel.Location = New System.Drawing.Point(552, 279)
+        Me.SpringConstantLabel.Name = "SpringConstantLabel"
+        Me.SpringConstantLabel.Size = New System.Drawing.Size(16, 17)
+        Me.SpringConstantLabel.TabIndex = 29
+        Me.SpringConstantLabel.Text = "0"
+        '
+        'SpringMassLabel
+        '
+        Me.SpringMassLabel.AutoSize = True
+        Me.SpringMassLabel.Location = New System.Drawing.Point(552, 236)
+        Me.SpringMassLabel.Name = "SpringMassLabel"
+        Me.SpringMassLabel.Size = New System.Drawing.Size(16, 17)
+        Me.SpringMassLabel.TabIndex = 30
+        Me.SpringMassLabel.Text = "0"
+        '
+        'EquilibriumLengthLabel
+        '
+        Me.EquilibriumLengthLabel.AutoSize = True
+        Me.EquilibriumLengthLabel.Location = New System.Drawing.Point(552, 195)
+        Me.EquilibriumLengthLabel.Name = "EquilibriumLengthLabel"
+        Me.EquilibriumLengthLabel.Size = New System.Drawing.Size(16, 17)
+        Me.EquilibriumLengthLabel.TabIndex = 31
+        Me.EquilibriumLengthLabel.Text = "0"
+        '
+        'AngleLabel
+        '
+        Me.AngleLabel.AutoSize = True
+        Me.AngleLabel.Location = New System.Drawing.Point(552, 153)
+        Me.AngleLabel.Name = "AngleLabel"
+        Me.AngleLabel.Size = New System.Drawing.Size(16, 17)
+        Me.AngleLabel.TabIndex = 32
+        Me.AngleLabel.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(410, 368)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(136, 17)
+        Me.Label7.TabIndex = 33
+        Me.Label7.Text = "x Pull Distance (cm):"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(406, 334)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(140, 17)
+        Me.Label9.TabIndex = 34
+        Me.Label9.Text = "Launch Speed (m/s):"
+        '
+        'xPullDistanceLabel
+        '
+        Me.xPullDistanceLabel.AutoSize = True
+        Me.xPullDistanceLabel.Location = New System.Drawing.Point(552, 368)
+        Me.xPullDistanceLabel.Name = "xPullDistanceLabel"
+        Me.xPullDistanceLabel.Size = New System.Drawing.Size(16, 17)
+        Me.xPullDistanceLabel.TabIndex = 35
+        Me.xPullDistanceLabel.Text = "0"
+        '
+        'LaunchSpeedLabel
+        '
+        Me.LaunchSpeedLabel.AutoSize = True
+        Me.LaunchSpeedLabel.Location = New System.Drawing.Point(552, 334)
+        Me.LaunchSpeedLabel.Name = "LaunchSpeedLabel"
+        Me.LaunchSpeedLabel.Size = New System.Drawing.Size(16, 17)
+        Me.LaunchSpeedLabel.TabIndex = 36
+        Me.LaunchSpeedLabel.Text = "0"
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Location = New System.Drawing.Point(701, 22)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(326, 269)
+        Me.CalculateButton.TabIndex = 37
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1077, 476)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.LaunchSpeedLabel)
+        Me.Controls.Add(Me.xPullDistanceLabel)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.AngleLabel)
+        Me.Controls.Add(Me.EquilibriumLengthLabel)
+        Me.Controls.Add(Me.SpringMassLabel)
+        Me.Controls.Add(Me.SpringConstantLabel)
+        Me.Controls.Add(Me.TargetHeightLabel)
+        Me.Controls.Add(Me.HorizontalDistanceLabel)
+        Me.Controls.Add(Me.ProjectileHeightLabel)
+        Me.Controls.Add(Me.SpringConstantButton)
+        Me.Controls.Add(Me.SpringMassButton)
+        Me.Controls.Add(Me.EquilibriumLenghtButton)
+        Me.Controls.Add(Me.AngleButton)
+        Me.Controls.Add(Me.HorizontalDistanceButton)
+        Me.Controls.Add(Me.TargetHeightButton)
+        Me.Controls.Add(Me.ProjectileHeightButton)
+        Me.Controls.Add(Me.SpringConstantTextBox)
+        Me.Controls.Add(Me.TargetHeightTextBox)
+        Me.Controls.Add(Me.HorizontalDistanceTextBox)
+        Me.Controls.Add(Me.AngleTextBox)
+        Me.Controls.Add(Me.EquilibriumLengthTextBox)
+        Me.Controls.Add(Me.SpringMassTextBox)
+        Me.Controls.Add(Me.ProjectileHeightTextBox)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Enter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents ProjectileHeightTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents SpringMassTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents EquilibriumLengthTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents AngleTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents HorizontalDistanceTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents TargetHeightTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents SpringConstantTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ProjectileHeightButton As System.Windows.Forms.Button
+    Friend WithEvents TargetHeightButton As System.Windows.Forms.Button
+    Friend WithEvents HorizontalDistanceButton As System.Windows.Forms.Button
+    Friend WithEvents AngleButton As System.Windows.Forms.Button
+    Friend WithEvents EquilibriumLenghtButton As System.Windows.Forms.Button
+    Friend WithEvents SpringMassButton As System.Windows.Forms.Button
+    Friend WithEvents SpringConstantButton As System.Windows.Forms.Button
+    Friend WithEvents ProjectileHeightLabel As System.Windows.Forms.Label
+    Friend WithEvents HorizontalDistanceLabel As System.Windows.Forms.Label
+    Friend WithEvents TargetHeightLabel As System.Windows.Forms.Label
+    Friend WithEvents SpringConstantLabel As System.Windows.Forms.Label
+    Friend WithEvents SpringMassLabel As System.Windows.Forms.Label
+    Friend WithEvents EquilibriumLengthLabel As System.Windows.Forms.Label
+    Friend WithEvents AngleLabel As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents xPullDistanceLabel As System.Windows.Forms.Label
+    Friend WithEvents LaunchSpeedLabel As System.Windows.Forms.Label
+    Friend WithEvents CalculateButton As System.Windows.Forms.Button
+
+End Class
---
+++ Spring Launcher Formula Calculator/Spring Launcher Formula Calculator/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+
+    Dim Hp, Ht, d, θ, i, m, k, v As Double
+    Const g As Double = 9.8
+
+    Private Sub ProjectileHeightButton_Click(sender As System.Object, e As System.EventArgs) Handles ProjectileHeightButton.Click
+        ProjectileHeightLabel.Text = "= " + Str(ProjectileHeightTextBox.Text) + " m"
+        Hp = Val(ProjectileHeightTextBox.Text)
+    End Sub
+
+    Private Sub TargetHeightButton_Click(sender As System.Object, e As System.EventArgs) Handles TargetHeightButton.Click
+        TargetHeightLabel.Text = "= " + Str(TargetHeightTextBox.Text) + " m"
+        Ht = Val(TargetHeightTextBox.Text)
+    End Sub
+
+    Private Sub HorizontalDistanceButton_Click(sender As System.Object, e As System.EventArgs) Handles HorizontalDistanceButton.Click
+        HorizontalDistanceLabel.Text = "= " + Str(HorizontalDistanceTextBox.Text) + " m"
+        d = Val(HorizontalDistanceTextBox.Text)
+    End Sub
+
+    Private Sub AngleButton_Click(sender As System.Object, e As System.EventArgs) Handles AngleButton.Click
+        AngleLabel.Text = "= " + Str(AngleTextBox.Text) + " degrees"
+        θ = Val(AngleTextBox.Text)
+    End Sub
+
+    Private Sub EquilibriumLenghtButton_Click(sender As System.Object, e As System.EventArgs) Handles EquilibriumLenghtButton.Click
+        EquilibriumLengthLabel.Text = "= " + Str(EquilibriumLengthTextBox.Text) + " m"
+        i = Val(EquilibriumLengthTextBox.Text)
+    End Sub
+
+    Private Sub SpringMassButton_Click(sender As System.Object, e As System.EventArgs) Handles SpringMassButton.Click
+        SpringMassLabel.Text = "= " + Str(SpringMassTextBox.Text) + " kg"
+        m = Val(SpringMassTextBox.Text)
+    End Sub
+
+    Private Sub SpringConstantButton_Click(sender As System.Object, e As System.EventArgs) Handles SpringConstantButton.Click
+        SpringConstantLabel.Text = "= " + Str(SpringConstantTextBox.Text) + " N/m"
+        k = Val(SpringConstantTextBox.Text)
+    End Sub
+
+    Private Sub CalculateButton_Click(sender As System.Object, e As System.EventArgs) Handles CalculateButton.Click
+        LaunchSpeedLabel.Text = LaunchSpeed(Hp, Ht, d, θ, i, m, k)
+        v = LaunchSpeed(Hp, Ht, d, θ, i, m, k)
+
+        If xPullDistancePlus(Hp, Ht, d, θ, i, m, k, v) > xPullDistanceMinus(Hp, Ht, d, θ, i, m, k, v) Then
+            xPullDistanceLabel.Text = xPullDistancePlus(Hp, Ht, d, θ, i, m, k, v) * 100
+        Else : xPullDistanceLabel.Text = xPullDistanceMinus(Hp, Ht, d, θ, i, m, k, v) * 100
+        End If
+
+    End Sub
+
+    Function LaunchSpeed(ByVal Hp As Double, Ht As Double, d As Double, θ As Double, i As Double, m As Double, k As Double) As Double
+        Return Math.Sqrt((g * d ^ 2) / (2 * ((Math.Cos(θ * Math.PI / 180)) ^ 2) * (d * Math.Tan(θ * Math.PI / 180) + Hp - Ht)))
+    End Function
+
+    Function xPullDistancePlus(ByVal Hp As Double, Ht As Double, d As Double, θ As Double, i As Double, m As Double, k As Double, v As Double) As Double
+        Return ((2 * m * g * Math.Sin(θ * Math.PI / 180)) + Math.Sqrt((4 * (m ^ 2) * (g ^ 2) * (Math.Sin(θ * Math.PI / 180) ^ 2)) + (4 * k * (m * (v ^ 2) + (2 * m * g * Math.Sin(θ * Math.PI / 180) * i))))) / (2 * k)
+    End Function
+
+    Function xPullDistanceMinus(ByVal Hp As Double, Ht As Double, d As Double, θ As Double, i As Double, m As Double, k As Double, v As Double) As Double
+        Return ((2 * m * g * Math.Sin(θ * Math.PI / 180)) - Math.Sqrt((4 * (m ^ 2) * (g ^ 2) * (Math.Sin(θ * Math.PI / 180) ^ 2)) + (4 * k * (m * (v ^ 2) + (2 * m * g * Math.Sin(θ * Math.PI / 180) * i))))) / (2 * k)
+    End Function
+
+End Class
